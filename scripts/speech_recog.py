31609229880e81f723bf35df90460e81919b86ab
BethMackey <bethany.mackey@brl.ac.uk>
2019-09-19 10:13:27 +0100

    Testing speech recognition

---
+++ scripts/speech_recog.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pepper_controller import PepperController
+
+
+import time
+
+robotIP = "westey.local" #Stevey
+PORT = 9559
+
+class SpeechRecog(PepperController):
+    
+    def fudge():
+        self.speechRecognition()
+        time.sleep(2)
+        self.onWordRecognized()
+
+
+if __name__ == '__main__':
+    task = SpeechRecog(robotIP, PORT)
+    task.fudge()
+    
